73999ee243c8f5e430eff40d338a53e435981d80
WolverMinion <mikeschmid71@googlemail.com>
2023-01-05 19:03:12 +0100

    fix: script failed with exit code 126

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 go test ./... -count=1 -cover -p 4 -coverprofile=coverage.out
 go tool cover -func coverage.out
 go tool cover -html=coverage.out -o cx-installer.html
